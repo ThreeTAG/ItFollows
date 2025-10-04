f243320fef52a74ff25c6e5f99b7a56f75e4c92c
Lucas Bandermann <lucasbandermann@threetag.net>
2025-10-04 18:50:10 +0200

    Fix #31, destroy splash potion upon impact

--- common/src/main/java/net/threetag/itfollows/entity/ThrownSplashPotionOfSpreading.java
+++ common/src/main/java/net/threetag/itfollows/entity/ThrownSplashPotionOfSpreading.java
@@ -1,5 +1,6 @@
 package net.threetag.itfollows.entity;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -30,31 +31,37 @@ public class ThrownSplashPotionOfSpreading extends ThrownSplashPotion {
 
     @Override
     protected void onHit(HitResult result) {
-        if (this.getOwner() instanceof ServerPlayer player) {
-            if (result instanceof EntityHitResult entityHit && entityHit.getEntity() instanceof ServerPlayer hitPlayer && player != hitPlayer) {
-                CursePlayerHandler.get(hitPlayer).startCurseInfectedBy(player);
-            } else if (result instanceof BlockHitResult blockHit) {
-                AABB aABB = this.getBoundingBox().move(blockHit.getLocation().subtract(this.position()));
-                AABB aABB2 = aABB.inflate(4.0, 2.0, 4.0);
-                List<LivingEntity> list = this.level().getEntitiesOfClass(LivingEntity.class, aABB2);
+        if (this.level() instanceof ServerLevel serverLevel) {
+            if (this.getOwner() instanceof ServerPlayer player) {
+                if (result instanceof EntityHitResult entityHit && entityHit.getEntity() instanceof ServerPlayer hitPlayer && player != hitPlayer) {
+                    CursePlayerHandler.get(hitPlayer).startCurseInfectedBy(player);
+                } else if (result instanceof BlockHitResult blockHit) {
+                    AABB aABB = this.getBoundingBox().move(blockHit.getLocation().subtract(this.position()));
+                    AABB aABB2 = aABB.inflate(4.0, 2.0, 4.0);
+                    List<LivingEntity> list = this.level().getEntitiesOfClass(LivingEntity.class, aABB2);
 
-                float g = ProjectileUtil.computeMargin(this);
-                if (!list.isEmpty()) {
+                    float g = ProjectileUtil.computeMargin(this);
+                    if (!list.isEmpty()) {
 
-                    for (LivingEntity livingEntity : list) {
-                        if (livingEntity.isAffectedByPotions()) {
-                            double d = aABB.distanceToSqr(livingEntity.getBoundingBox().inflate(g));
-                            if (d < 16.0) {
+                        for (LivingEntity livingEntity : list) {
+                            if (livingEntity.isAffectedByPotions()) {
+                                double d = aABB.distanceToSqr(livingEntity.getBoundingBox().inflate(g));
+                                if (d < 16.0) {
 
-                                if (livingEntity instanceof ServerPlayer hitPlayer && player != hitPlayer) {
-                                    CursePlayerHandler.get(hitPlayer).startCurseInfectedBy(player);
-                                    return;
+                                    if (livingEntity instanceof ServerPlayer hitPlayer && player != hitPlayer) {
+                                        CursePlayerHandler.get(hitPlayer).startCurseInfectedBy(player);
+                                        return;
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
+
+            int i = 2007;
+            serverLevel.levelEvent(i, this.blockPosition(), 7561558);
+            this.discard();
         }
     }
 }
